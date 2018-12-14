d4df3e8a66b89d9928f0cff09392b166e6cba59f
JMP <jorgemprates@gmail.com>
2018-12-14 13:49:55 -0200

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/jmp/cursomc/domain/Categoria.java
+++ src/main/java/com/jmp/cursomc/domain/Categoria.java
@@ -2,39 +2,54 @@ package com.jmp.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	
+
 	public Categoria() {
 	}
- 	public Categoria(Integer id, String nome) {
+
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
- 	public Integer getId() {
+
+	public Integer getId() {
 		return id;
 	}
- 	public void setId(Integer id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
- 	public String getNome() {
+
+	public String getNome() {
 		return nome;
 	}
- 	public void setNome(String nome) {
+
+	public void setNome(String nome) {
 		this.nome = nome;
 	}
- 	@Override
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
- 	@Override
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -50,4 +65,4 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
- }
+}
