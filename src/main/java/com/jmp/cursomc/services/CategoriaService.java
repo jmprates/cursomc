4281b0c7becb41f2be0381a8d0b373fadc041f44
JMP <jorgemprates@gmail.com>
2018-12-14 23:15:16 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/jmp/cursomc/services/CategoriaService.java
+++ src/main/java/com/jmp/cursomc/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.jmp.cursomc.domain.Categoria;
 import com.jmp.cursomc.repositories.CategoriaRepository;
+import com.jmp.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
