6923dfcc47bb1453458c3bfba269c5e5a6e22928
JMP <jorgemprates@gmail.com>
2018-12-16 03:02:30 -0200

    ItemPedido e ItemPedidoPK

--- src/main/java/com/jmp/cursomc/CursomcApplication.java
+++ src/main/java/com/jmp/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.jmp.cursomc.domain.Cidade;
 import com.jmp.cursomc.domain.Cliente;
 import com.jmp.cursomc.domain.Endereco;
 import com.jmp.cursomc.domain.Estado;
+import com.jmp.cursomc.domain.ItemPedido;
 import com.jmp.cursomc.domain.Pagamento;
 import com.jmp.cursomc.domain.PagamentoComBoleto;
 import com.jmp.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.jmp.cursomc.repositories.CidadeRepository;
 import com.jmp.cursomc.repositories.ClienteRepository;
 import com.jmp.cursomc.repositories.EnderecoRepository;
 import com.jmp.cursomc.repositories.EstadoRepository;
+import com.jmp.cursomc.repositories.ItemPedidoRepository;
 import com.jmp.cursomc.repositories.PagamentoRepository;
 import com.jmp.cursomc.repositories.PedidoRepository;
 import com.jmp.cursomc.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ import com.jmp.cursomc.repositories.ProdutoRepository;
 		private PedidoRepository pedidoRepository;
 		@Autowired
 		private PagamentoRepository pagamentoRepository;
+		@Autowired
+		private ItemPedidoRepository itemPedidoRepository;
 		
 		public static void main(String[] args) {
 			SpringApplication.run(CursomcApplication.class, args);
@@ -114,6 +118,19 @@ import com.jmp.cursomc.repositories.ProdutoRepository;
 					
 			pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 			pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+			
+			ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+			ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+			ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+			
+			ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+			ped2.getItens().addAll(Arrays.asList(ip3));
+			
+			p1.getItens().addAll(Arrays.asList(ip1));
+			p2.getItens().addAll(Arrays.asList(ip3));
+			p3.getItens().addAll(Arrays.asList(ip2));
+			
+			itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));		
 			
 		}
 
--- src/main/java/com/jmp/cursomc/domain/Pedido.java
+++ src/main/java/com/jmp/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.jmp.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,73 +12,98 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
- 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
-	
-	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
+
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
- 	@ManyToOne
-	@JoinColumn(name="cliente_id")
-	private Cliente cliente;
-	
 	@ManyToOne
-	@JoinColumn(name="endereco_de_entrega_id")
+	@JoinColumn(name = "cliente_id")
+	private Cliente cliente;
+
+	@ManyToOne
+	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
-	
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
 	public Pedido() {
 	}
- 	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
+
+	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
 		this.id = id;
 		this.instante = instante;
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
- 	public Integer getId() {
+
+	public Integer getId() {
 		return id;
 	}
- 	public void setId(Integer id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
- 	public Date getInstante() {
+
+	public Date getInstante() {
 		return instante;
 	}
- 	public void setInstante(Date instante) {
+
+	public void setInstante(Date instante) {
 		this.instante = instante;
 	}
- 	public Pagamento getPagamento() {
+
+	public Pagamento getPagamento() {
 		return pagamento;
 	}
- 	public void setPagamento(Pagamento pagamento) {
+
+	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
- 	public Cliente getCliente() {
+
+	public Cliente getCliente() {
 		return cliente;
 	}
- 	public void setCliente(Cliente cliente) {
+
+	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
- 	public Endereco getEnderecoDeEntrega() {
+
+	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
- 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
+
+	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
- 	@Override
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
- 	@Override
+
+	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
@@ -92,7 +119,5 @@ public class Pedido implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
